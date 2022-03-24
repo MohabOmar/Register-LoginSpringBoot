59567446f1a38bc4bbd075036609acae611f76dd
MohabOmar <58468897+MohabOmar@users.noreply.github.com>
2022-03-24 09:39:59 +0200

    Create UserService.java

---
+++ src/main/java/com/example/demo/UserService.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author mohab
+ */
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepo userRepository;
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public User findUserByUserName(String userName) {
+        return userRepository.findByUserName(userName);
+    }
+
+    public User saveUser(User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));;
+
+        return userRepository.save(user);
+    }
+
+}
